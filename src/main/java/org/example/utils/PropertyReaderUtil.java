6156f4edcde22b7b3fa10d4ede7e3446a3df6a78
Neharana17 <67993738+Neharana17@users.noreply.github.com>
2023-11-06 17:03:36 +0530

    Update PropertyReaderUtil.java

--- src/main/java/org/example/utils/PropertyReaderUtil.java
+++ src/main/java/org/example/utils/PropertyReaderUtil.java
@@ -1,4 +1,22 @@
 package org.example.utils;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class PropertyReaderUtil {
 }
+public static String readKey(String key) throws Exception {
+        FileInputStream fileInputStream = new FileInputStream(new File(System.getProperty("user.dir") + "/src/main/java/org/example/resource/TDP.properties"));
+        Properties p= new Properties();
+        p.load(fileInputStream);
+
+        if(p.getProperty(key)==null){
+            throw new Exception("Not able to find key");
+        }else{
+           return p.getProperty(key);
+        }
+    }
+}
+
