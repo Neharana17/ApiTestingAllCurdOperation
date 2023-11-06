a9129285b911efc5606728afc087a40c066213dd
Neharana17 <67993738+Neharana17@users.noreply.github.com>
2023-11-06 17:02:14 +0530

    Update FillowUtil.java

--- src/main/java/org/example/utils/FillowUtil.java
+++ src/main/java/org/example/utils/FillowUtil.java
@@ -1,4 +1,19 @@
 package org.example.utils;
 
 public class FillowUtil {
+  public static String FilePath=System.getProperty("user.dir")+"/src/main/java/org/example/resource/TestData.xlsx";
+
+    public static String fatchDatafromXLSX(String sheetName ,String id, String FieldName) throws FilloException {
+        String value=null;
+        Fillo fillo=new Fillo();
+        Connection connection= fillo.getConnection(FilePath);
+        String query = "Select * from " + sheetName + " " + "where ID='" + id + " ";
+        Recordset recordset=connection.executeQuery(query);
+        while (recordset.next()){
+            value=recordset.getField(FieldName);
+        }
+        recordset.close();
+        connection.close();
+        return value;
+    }
 }
